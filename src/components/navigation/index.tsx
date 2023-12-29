fc10172b0c33072e5c5f40b09a21b1d7f2949f20
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-12-29 23:31:40 +0700

    add favicon

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -6,13 +6,14 @@ import NavigationItem from "./navigation-item"
 import { Button } from "../ui/button"
 import { LogOut } from "lucide-react"
 import useAdminStore from "@/store/useAdminStore"
+import Logo from "../logo"
 
 const NavigationBar = () => {
     const { logout } = useAdminStore()
     return (
         <div className='flex h-full w-full flex-col justify-between p-2 shadow-md'>
             <Link to={ROUTE.BASE} className='mb-8 flex justify-center'>
-                <img src={"/vite.svg"} alt={"Next.js"} width={100} height={100} />
+                <Logo width={200} height={40} color='var(--primary)' className='fill-primary' />
             </Link>
             <div className='flex flex-1 flex-col gap-1'>
                 {navigationMocks.map((navigation) => (
